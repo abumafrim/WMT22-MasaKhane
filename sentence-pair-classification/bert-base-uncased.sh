727624351c2a2528766462adb59602e6492c1f6e
abumafrim <abumafrim@gmail.com>
2022-06-03 09:45:17 +0100

    multiple updates

--- sentence-pair-classification/bert-base-uncased.sh
+++ sentence-pair-classification/bert-base-uncased.sh
@@ -1,28 +1,14 @@
-export MAX_LENGTH=128
 export MODEL=bert-base-uncased
 export TRAIN_DATA=data/en-hau/spc-en_hau_train.tsv
 export VAL_DATA=data/en-hau/spc-en_hau_dev.tsv
 export TEST_DATA=data/en-hau/spc-en_hau_test.tsv
-export BATCH_SIZE=16
-export NUM_EPOCHS=2
-export LEARNING_RATE=2e-5
-export FREEZE_BERT=False
-export ITERS_TO_ACCUMULATE=2
-export PRED_THRESHOLD=0.5
-export SEED=1
+export NUM_EPOCHS=4
 
 python3 run-sp-class.py \
     --train \
     --eval=True \
     --model=$MODEL \
-    --epochs=$NUM_EPOCHS \
     --train_data=$TRAIN_DATA \
     --val_data=$VAL_DATA \
     --test_data=$TEST_DATA \
-    --freeze_bert=$FREEZE_BERT \
-    --maxlen=$MAX_LENGTH \
-    --batch_size=$BATCH_SIZE \
-    --learning_rate=$LEARNING_RATE \
-    --iters_to_accumulate=$ITERS_TO_ACCUMULATE \
-    --pred_threshold=$PRED_THRESHOLD \
-    --seed=$SEED
+    --epochs=$NUM_EPOCHS
--- sentence-pair-classification/predict.sh
+++ sentence-pair-classification/predict.sh
@@ -1,18 +1,14 @@
-export MAX_LENGTH=128
 export MODEL=albert-base-v2
 export MODEL_PATH=models/albert-base-v2_lr_2e-05_val_loss_0.15311_ep_1.pt
 export DATA_PATH=data/en-hau/spc-en_hau_10000_to_pred.tsv
 export OUTPUT_PATH=results/
 export BATCH_SIZE=32
 export PRED_THRESHOLD=0.5
-export SEED=2
 
 python3 predict.py \
     --predict \
+    --model=$MODEL \
     --model_path=$MODEL_PATH \
     --data_path=$DATA_PATH \
     --output_path=$OUTPUT_PATH \
-    --maxlen=$MAX_LENGTH \
-    --batch_size=$BATCH_SIZE \
     --pred_threshold=$PRED_THRESHOLD \
-    --seed=$SEED
