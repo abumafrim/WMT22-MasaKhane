f3ae8939f635a4d2c08a5c0ed7f5949efa0c705b
abumafrim <abumafrim@gmail.com>
2022-10-17 18:39:46 +0100

    Create train.sh

---
+++ sentence-pair-classification/scripts/train.sh
@@ -0,0 +1,20 @@
+model="albert-large-v2"
+
+for lang in eng-hau,eng-ibo,eng-lug,eng-swh,eng-tsn,eng-yor,eng-zul,fra-wol; do
+    train=data/$lang/spc-$lang"_train.tsv"
+    dev=data/$lang/spc-$lang"_dev.tsv"
+    test=data/$lang/spc-$lang"_test.tsv"
+
+    model_path=models/$model/$lang
+    mkdir -p $model_path
+
+    python run-sp-class.py \
+        --train \
+        --eval=True \
+        --model=$model \
+        --model_path=$model_path \
+        --train_data=$train \
+        --val_data=$dev \
+        --test_data=$test \
+        --epochs=4
+done
