cb47feb88c9e1a16164e45c44559d45c0f9ac26b
abumafrim <abumafrim@gmail.com>
2022-10-18 17:00:13 +0100

    Update predict.sh

--- sentence-pair-classification/scripts/predict.sh
+++ sentence-pair-classification/scripts/predict.sh
@@ -1,12 +1,12 @@
 auto_aligned=('wmt22_african' 'lava-corpus' 'webcrawl_african' 'WikiMatrix' 'CCAligned' 'CCMatrix' 'ParaCrawl' 'GNOME' 'KDE4' 'TED2020' 'XLEnt' 'Ubuntu' 'wikimedia' 'MultiCCAligned')
 
 model=albert-xlarge-v2
-base_path=../../data/filtering
-models_path=../models
+basepath=../../data/filtering
+modelspath=../models
 #very large loss (arbitrary)
-val_loss=1000
+valloss=1000
 
-filename=$base_path/$model'-done_pred.txt'
+filename=$basepath/$model'-done_pred.txt'
 IFS=$'\r\n' GLOBIGNORE='*' command eval  'done_pred=($(cat filename))'
 
 echo ${done_pred[@]}
