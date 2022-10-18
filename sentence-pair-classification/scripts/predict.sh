bf5c0a5aec0dd8ba4a87273783b7fed3caf4115f
abumafrim <abumafrim@gmail.com>
2022-10-18 18:39:06 +0100

    Update predict.sh

--- sentence-pair-classification/scripts/predict.sh
+++ sentence-pair-classification/scripts/predict.sh
@@ -1,19 +1,25 @@
-auto_aligned=('wmt22_african' 'lava-corpus' 'webcrawl_african' 'WikiMatrix' 'CCAligned' 'CCMatrix' 'ParaCrawl' 'GNOME' 'KDE4' 'TED2020' 'XLEnt' 'Ubuntu' 'wikimedia' 'MultiCCAligned')
-
 model=albert-xlarge-v2
 basepath=../../data/filtering
 modelspath=../models
 #very large loss (arbitrary)
 valloss=1000
 
-filename=$basepath/$model"-done_pred.txt"
-readarray -t donepred < $filename
-value=wmt22_african$'\t'eng-hau.tsv
-
-if [[ " ${donepred[*]} " =~ " ${value} " ]]; then
-    echo "Finished: $filename"
+donefile=$basepath/$model"-done_pred.txt"
+if [ -f $donefile ]; then
+    readarray -t donepred < $donefile
 fi
 
-if [[ ! " ${donepred[*]} " =~ " ${value} " ]]; then
-    echo "ToDo: $filename"
-fi
+for data in wmt22_african lava-corpus webcrawl_african WikiMatrix CCAligned CCMatrix ParaCrawl GNOME KDE4 TED2020 XLEnt Ubuntu wikimedia MultiCCAligned; do
+  datapath=$basepath/$data
+  for sfile in $(ls -d $datapath */); do
+    value=$data$'\t'$sfile
+    if [[ ! " ${donepred[*]} " =~ " ${value} " ]]; then
+        lang=${sfile:0:7}
+        echo "Predicting the quality of: $lang"
+
+    elif [[ " ${donepred[*]} " =~ " ${value} " ]]; then
+	    echo "$value finished."
+    fi
+  done
+  echo ''
+done
