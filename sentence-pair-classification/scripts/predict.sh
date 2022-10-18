249c1f4469bffb5e76f7489f16d4edd62474c4b9
abumafrim <abumafrim@gmail.com>
2022-10-18 17:12:06 +0100

    Update predict.sh

--- sentence-pair-classification/scripts/predict.sh
+++ sentence-pair-classification/scripts/predict.sh
@@ -7,6 +7,6 @@ modelspath=../models
 valloss=1000
 
 filename=$basepath/$model"-done_pred.txt"
-IFS=$'\r\n' GLOBIGNORE='*' command eval 'donepred=($(cat filename))'
+readarray -t donepred < $filename
 
 echo ${donepred[@]}
