bcaddc5560bf55557d0c2f59db6c3d91552b9f7d
abumafrim <abumafrim@gmail.com>
2022-10-18 21:21:04 +0100

    Update predict.sh

--- sentence-pair-classification/scripts/predict.sh
+++ sentence-pair-classification/scripts/predict.sh
@@ -9,9 +9,33 @@ if [ -f $donefile ]; then
     readarray -t donepred < $donefile
 fi
 
-value=webcrawl_african' 'eng-hau
+for data in wmt22_african lava-corpus webcrawl_african WikiMatrix CCAligned CCMatrix ParaCrawl GNOME KDE4 TED2020 XLEnt Ubuntu wikimedia MultiCCAligned; do
+  datapath=$basepath/$data
+  for sfile in $datapath/*.tsv; do
+    lang="$(cut -d'/' -f6 <<<"$sfile")"
+    lang=${lang//.tsv}
+    value=$data$' '$lang
+    if [[ ! " ${donepred[*]} " =~ " ${value} " ]]; then
+        model_path=$modelspath/$model/$lang
+        for x in $model_path/$model*.pt; do
+            loss="$(cut -d'_' -f6 <<<"$x")"
+            if awk "BEGIN {exit !($loss < $val_loss)}"; then
+                model_path=$x
+            else
+                model_path=""
+            fi
+        done
 
-if [[ ! " ${donepred[*]} " =~ " ${value} " ]]; then
-    echo "$value"
-    echo "Not found"
-fi
+        data_to_classify=$sfile
+        save_to=$datapath/$model"_"$lang".preds"
+
+        echo "Finished: $data $lang"
+
+        echo $value >> $donefile
+
+    elif [[ " ${donepred[*]} " =~ " ${value} " ]]; then
+	    echo "Finished: $data $lang already"
+    fi
+  done
+  echo ''
+done
